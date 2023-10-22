1444513e9f413d1aca3af0142b65fbfc1fedd04e
goose <diomns2006@yandex.ru>
2023-10-22 23:59:56 +0300

    Fix all path

--- RUpassport/RUpassport.py
+++ RUpassport/RUpassport.py
@@ -7,20 +7,23 @@ import cv2
 class Pasport:
     def __init__(self):
         self.__reader = easyocr.Reader(['ru'])
-        self.__model = YOLO(os.path.abspath("pasport_model.pt"))
+        module_dir = os.path.dirname(__file__)
+        model_file_path = os.path.join(module_dir, 'pasport_model.pt')
+        self.__model = YOLO(model_file_path)
 
-    def recognize_pasport(self, img: str, id: str):
+    def recognize_pasport(self, img: str, main_folder: str, folder_id: str):
         """
         :param img: path to passport img
-        :param id: folder id (random value)
+        :param main_folder: An intermediate folder for storing recognized photos, after returning the data is deleted
+        :param folder_id: id of the folder inside main_folder where the photo recognition is stored (random value)
         :return: info from passport
         """
-        self.__model(img, retina_masks=True, save_crop=True, project="file", name=id)
+        self.__model(img, retina_masks=True, save_crop=True, project=main_folder, name=folder_id)
 
         info = {}
-        for dir in os.listdir(f"file/{id}/crops"):
+        for dir in os.listdir(f"{main_folder}/{folder_id}/crops"):
             if dir != "pass-ort":
-                info[dir] = f"file/{id}/crops/{dir}/" + os.listdir(f"file/123/crops/{dir}/")[0]
+                info[dir] = f"{main_folder}/{folder_id}/crops/{dir}/" + os.listdir(f"{main_folder}/{folder_id}/crops/{dir}/")[0]
 
         img = cv2.imread(info["num"])
         img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
@@ -32,5 +35,5 @@ class Pasport:
         for i in info.keys():
             info[i] = "".join(self.__reader.readtext(info[i], detail=0)).lower().capitalize()
 
-        shutil.rmtree(f"file/{id}")
+        shutil.rmtree(f"{main_folder}/{folder_id}")
         return info
