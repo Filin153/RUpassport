a0402069a0195c3829c1ae7c553e48f376bfe2ad
goose <diomns2006@yandex.ru>
2023-11-08 22:30:09 +0300

    Fix none passport

--- RUpassport/RUpassport.py
+++ RUpassport/RUpassport.py
@@ -19,32 +19,35 @@ class Pasport:
         :param folder_id: id of the folder inside main_folder where the photo recognition is stored (random value)
         :return: info from passport
         """
-        self.__model(img, retina_masks=True, save_crop=True, project=main_folder, name=folder_id)
+        try:
+            self.__model(img, retina_masks=True, save_crop=True, project=main_folder, name=folder_id)
 
-        info = {}
-        for dir in os.listdir(f"{main_folder}/{folder_id}/crops"):
-            if dir != "pass-ort":
-                info[dir] = f"{main_folder}/{folder_id}/crops/{dir}/" + os.listdir(f"{main_folder}/{folder_id}/crops/{dir}/")[0]
+            info = {}
+            for dir in os.listdir(f"{main_folder}/{folder_id}/crops"):
+                if dir != "pass-ort":
+                    info[dir] = f"{main_folder}/{folder_id}/crops/{dir}/" + os.listdir(f"{main_folder}/{folder_id}/crops/{dir}/")[0]
 
-        img = cv2.imread(info["num"])
-        img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
-        cv2.imwrite(info["num"], img)
-        img = cv2.imread(info["ser"])
-        img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
-        cv2.imwrite(info["ser"], img)
-
-
-        for i in info.keys():
-            info[i] = "".join(self.__reader.readtext(info[i], detail=0)).lower().capitalize()
-
-        info['date'] = self.made_date(info['data'])
-        info['out_date'] = self.made_date(info['out_data'])
-        info.pop('data')
-        info.pop('out_data')
+            img = cv2.imread(info["num"])
+            img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            cv2.imwrite(info["num"], img)
+            img = cv2.imread(info["ser"])
+            img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            cv2.imwrite(info["ser"], img)
 
 
-        shutil.rmtree(f"{main_folder}/{folder_id}")
-        return info
+            for i in info.keys():
+                info[i] = "".join(self.__reader.readtext(info[i], detail=0)).lower().capitalize()
+
+            info['date'] = self.made_date(info['data'])
+            info['out_date'] = self.made_date(info['out_data'])
+            info.pop('data')
+            info.pop('out_data')
+
+
+            shutil.rmtree(f"{main_folder}/{folder_id}")
+            return info
+        except Exception:
+            return {}
 
     @staticmethod
     def made_date(line: str) -> str:
